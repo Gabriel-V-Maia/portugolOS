011296c483bc027dfb8ccf5b73c7491eb4905c3a
Gabriel-V-Maia <gabrielvinicius.damaia@gmail.com>
2026-01-31 16:46:51 -0300

    futura VFS

--- kernel/vfs/vfs.c
+++ kernel/vfs/vfs.c
@@ -1,18 +1,18 @@
 #include "vfs.h"
 
-typedef struct file 
+typedef struct file
 {
     char nome[30];
     char* data;
-    struct folder* parent; 
+    struct folder* parent;
 } file;
 
-typedef struct folder 
+typedef struct folder
 {
     char nome[30];
     struct folder* parent;
-    
-    file** files;  
+    file** files;
+
     unsigned int filecount;
-} folder; 
+} folder;
 
