7422847d63ef25125886fff418953a3f2fdcb0da
Gabriel-V-Maia <gabrielvinicius.damaia@gmail.com>
2026-02-01 02:03:09 -0300

    VFS pela metade

--- kernel/vfs/vfs.c
+++ kernel/vfs/vfs.c
@@ -1,18 +1,99 @@
 #include "vfs.h"
+#include "../mm/memory.h"
+#include "../libs/string.h"
+#include "../libs/kprintf.h"
 
-typedef struct file
+static folder* root = NULL;
+
+void init_vfs()
 {
-    char nome[30];
-    char* data;
-    struct folder* parent;
-} file;
+    root = (folder*)malloc(sizeof(folder));
+    
+    strcpy(root->nome, "/");
+    root->parent = NULL;
+    root->files = NULL;
+    root->filecount = 0;
+}
 
-typedef struct folder
+vfs_cursor* init_cursor()
 {
-    char nome[30];
-    struct folder* parent;
-    file** files;
+    vfs_cursor* cursor = (vfs_cursor*)malloc(sizeof(vfs_cursor));
 
-    unsigned int filecount;
-} folder;
+    cursor->busy = 0;
+    strcpy(cursor->taskDescription, "");
+    strcpy(cursor->currentPath, "/");
+    cursor->current_folder = root;
 
+    return cursor; 
+}
+
+void create_file(vfs_cursor* cursor, char* nome, char* conteudo)
+{
+    cursor->busy = 1;
+    strcpy(cursor->taskDescription, "Criando arquivo...");
+    
+    folder* pasta = cursor->current_folder;
+    
+    pasta->filecount++;
+    
+    if (pasta->files == NULL) {
+        pasta->files = (file**)malloc(sizeof(file*));
+    } else {
+        file** novo = (file**)malloc(pasta->filecount * sizeof(file*));
+        
+        for (int i = 0; i < pasta->filecount - 1; i++) {
+            novo[i] = pasta->files[i];
+        }
+        
+        free(pasta->files);
+        pasta->files = novo;
+    }
+    
+    file* novo_arquivo = (file*)malloc(sizeof(file));
+    strcpy(novo_arquivo->nome, nome);
+    novo_arquivo->parent = pasta;
+    
+    int tam = 0;
+    while (conteudo[tam]) tam++;  
+    
+    novo_arquivo->data = (char*)malloc(tam + 1);
+    strcpy(novo_arquivo->data, conteudo);
+    
+    pasta->files[pasta->filecount - 1] = novo_arquivo;
+    
+    cursor->busy = 0;
+    strcpy(cursor->taskDescription, "");
+}
+
+void list_files(vfs_cursor* cursor)
+{
+    folder* pasta = cursor->current_folder;
+    
+    kprintf("Arquivos em %s:\n", cursor->currentPath);
+    
+    for (unsigned int i = 0; i < pasta->filecount; i++) {
+        kprintf("  - %s\n", pasta->files[i]->nome);
+    }
+}
+
+void test_vfs()
+{
+    
+    init_vfs();
+    
+    if (root == NULL) {
+        kprintf("ERRO: root eh NULL!\n");
+        return;
+    }
+    
+    kprintf("Root OK. Criando cursor...\n");
+    
+    vfs_cursor* cursor = init_cursor();
+    
+    if (cursor == NULL) {
+        kprintf("ERRO: cursor eh NULL!\n");
+        return;
+    }
+    
+    kprintf("Cursor OK / ROOT OK.\n");
+}
--- kernel/vfs/vfs.h
+++ kernel/vfs/vfs.h
@@ -1,10 +1,35 @@
 #ifndef VFS_H
 #define VFS_H
 
-// typedef struct cursor cursor;
-
-typedef struct file file;
 typedef struct folder folder;
 
+typedef struct
+{
+    char nome[32];
+    char* data;
+    folder* parent;
+} file;
+
+typedef struct folder
+{
+    char nome[32];
+    folder* parent;
+    file** files;
+    unsigned int filecount;
+} folder;
+
+typedef struct {
+    char currentPath[64];
+    int busy;
+    char taskDescription[128];
+    folder* current_folder;
+} vfs_cursor;
+
+void init_vfs();
+vfs_cursor* init_cursor();
+void create_file(vfs_cursor* cursor, char* nome, char* conteudo);
+void list_files(vfs_cursor* cursor);
+
+void test_vfs();
 
 #endif
