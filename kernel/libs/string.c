5bfc4158a3e760bd491802715e4078452b7cba59
Gabriel Vinícius da Maia <gabrielvinicius.damaia@gmail.com>
2026-04-13 08:32:05 -0300

    feat: add memset, memcpy, and memmove functions

--- kernel/libs/string.c
+++ kernel/libs/string.c
@@ -34,3 +34,33 @@ char* strcat(char* dst, const char* src)
     return dst;
 }
 
+void* memset(void* ptr, int value, size_t size)
+{
+    char* p = (char*)ptr;
+    for (size_t i = 0; i < size; i++)
+        p[i] = (char)value;
+    return ptr;
+}
+
+void* memcpy(void* dst, const void* src, size_t size)
+{
+    char* d = (char*)dst;
+    const char* s = (const char*)src;
+    for (size_t i = 0; i < size; i++)
+        d[i] = s[i];
+    return dst;
+}
+
+void* memmove(void* dst, const void* src, size_t size)
+{
+    char* d = (char*)dst;
+    const char* s = (const char*)src;
+    if (d < s) {
+        for (size_t i = 0; i < size; i++)
+            d[i] = s[i];
+    } else {
+        for (size_t i = size; i > 0; i--)
+            d[i-1] = s[i-1];
+    }
+    return dst;
+}
